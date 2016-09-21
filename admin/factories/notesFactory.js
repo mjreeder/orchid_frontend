da9c8ce1c7797fc662b72e27052ca937d6f3f6bb
Matt Reeder <mjreeder@bsu.edu>
2016-09-21 10:12:57 -0400

    Create notes factory

---
+++ admin/factories/notesFactory.js
@@ -0,0 +1,40 @@
+orchid.factory('NotesFactory', function($http) {
+
+    var data = {};
+    var baseUrl = 'http://localhost:8888/orchid_site/public/api/notes';
+
+    data.getAllNotes = function() {
+        $http.get(baseUrl);
+    }
+
+    data.getNoteById = function(plant_id) {
+        $http.get(baseUrl + "/plant_id" + plant_id);
+    }
+
+    data.createNote = function (note) {
+      $http({
+          method: "POST",
+          url: baseUrl + '/create',
+          data: {
+              "plant_id": note.plantId,
+              "note": note.text,
+              "timestamp": note.timestamp
+          }
+      });
+    }
+
+    data.editNote = function(note){
+      $http({
+          method: "PUT",
+          url: baseUrl + '/update',
+          data: {
+              "plant_id": note.plantId,
+              "note": note.text,
+              "timestamp": note.timestamp,
+              "id": note.id
+          }
+      });
+    }
+
+    return data;
+});
