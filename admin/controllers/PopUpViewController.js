3f87e8108d4298a6ac8bf602f110ddebba95612a
Seth Winslow <swwinlow@bsu.eudu>
2016-09-26 11:45:19 -0400

    Fixed Pop Up forum

--- admin/controllers/PopUpViewController.js
+++ admin/controllers/PopUpViewController.js
@@ -3,4 +3,9 @@ app.controller('PopUpViewController', function(CONFIG, $scope, $location){
 
     $scope.today = new Date();
 
+    $scope.submitPopUp = function(){
+        console.log("HELLO");
+        $location.path('#/search');
+    }
+
 });
