de8bc3eb1314d6b855eb7a45bd5fa7bd16682689
Lucas Smith <friedmicrochips@gmail.com>
2016-09-29 17:01:58 -0400

    Create functionality to get data for popup controller using multiple connections.

--- admin/controllers/PopUpViewController.js
+++ admin/controllers/PopUpViewController.js
@@ -1,10 +1,11 @@
-app.controller('PopUpViewController', function(CONFIG, $scope, $location, $rootScope){
+app.controller('PopUpViewController', function(CONFIG, $scope, $location, $rootScope, BloomingFactory, SprayedFactory, PottingFactory, HealthFactory){
 
     $scope.plant = {};
 
     $scope.$on('current-plant', function(event, data){
       console.log(data);
       $scope.plant = data;
+      init();
     })
 
     $scope.today = new Date();
@@ -17,5 +18,32 @@ app.controller('PopUpViewController', function(CONFIG, $scope, $location, $rootS
     $scope.closePopUp = function(){
       $rootScope.$broadcast('popup-close', true);
     }
+
+    //These requests are async, but since they do not equire each other's data we will execute them concurently
+    var init = function(){
+      BloomingFactory.getBloomByPlantID($scope.plant.id).then(function(data){
+        concatObjects(data);
+      })
+      SprayedFactory.getPestByPlantID($scope.plant.id).then(function(data){
+        concatObjects(data);
+      })
+      PottingFactory.getBloomByPlantID($scope.plant.id).then(function(data){
+        concatObjects(data);
+      })
+      HealthFactory.getHealthBtPlantID($scope.plant.id).then(function(data){
+        concatObjects(data);
+      })
+    }
+
+    //Add data to scope object
+    var concatObjects = function(data){
+      for(key in data){
+        if(data.hasOwnProperty(key)){
+          $scope[key] = data[key];
+        }
+      }
+    }
+
+
 
 });
