83a2274ab98567b9879c3c27873875d9d2a26a93
Lucas Smith <friedmicrochips@gmail.com>
2016-09-30 14:52:30 -0400

    Map variable names to the backend.

--- admin/controllers/PopUpViewController.js
+++ admin/controllers/PopUpViewController.js
@@ -1,6 +1,7 @@
 app.controller('PopUpViewController', function(CONFIG, $scope, $location, $rootScope, BloomingFactory, SprayedFactory, PottingFactory, HealthFactory){
 
     $scope.plant = {};
+    $scope.health_condition = "";
 
     $scope.$on('current-plant', function(event, data){
       console.log(data);
@@ -11,8 +12,7 @@ app.controller('PopUpViewController', function(CONFIG, $scope, $location, $rootS
     $scope.today = new Date();
 
     $scope.submitPopUp = function(){
-        console.log("HELLO");
-        $location.path('#/search');
+
     }
 
     $scope.closePopUp = function(){
@@ -22,24 +22,26 @@ app.controller('PopUpViewController', function(CONFIG, $scope, $location, $rootS
     //These requests are async, but since they do not equire each other's data we will execute them concurently
     var init = function(){
       BloomingFactory.getBloomByPlantID($scope.plant.id).then(function(data){
-        concatObjects(data);
+        concatObjects(data, 'blooming');
       })
       SprayedFactory.getPestByPlantID($scope.plant.id).then(function(data){
-        concatObjects(data);
+        concatObjects(data, 'sprayed');
       })
       PottingFactory.getBloomByPlantID($scope.plant.id).then(function(data){
-        concatObjects(data);
+        concatObjects(data, 'potting');
       })
       HealthFactory.getHealthBtPlantID($scope.plant.id).then(function(data){
-        concatObjects(data);
+        concatObjects(data, 'health');
       })
     }
 
+    $scope.star
+
     //Add data to scope object
-    var concatObjects = function(data){
+    var concatObjects = function(data, prefix){
       for(key in data){
         if(data.hasOwnProperty(key)){
-          $scope[key] = data[key];
+          $scope[prefix + '_' + key] = data[key];
         }
       }
     }
