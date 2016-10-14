93b36ccb95eba48ce1cf39540c8bb72fc4a5c3c7
Matt Reeder <mjreeder@bsu.edu>
2016-10-14 11:32:00 -0400

    Save session so users don't have to login on reload

--- admin/controllers/LoginViewController.js
+++ admin/controllers/LoginViewController.js
@@ -1,9 +1,10 @@
-app.controller('LoginViewController', function($scope, $rootScope, $location, SessionFactory, CONFIG){
+app.controller('LoginViewController', function($scope, $rootScope, $location, SessionFactory, sessionService, CONFIG){
   $scope.login = function (credentials) {
     // console.log(credentials);
 
     SessionFactory.login(credentials).then(function (response) {
       $rootScope.userSession = response.data.data.session_key;
+      sessionService.createSessionCookie(credentials.username, response.data.data.session_key)
       $location.path($rootScope.redirect);
     });
 
