9aac97ebd22cd6314e593072cb91180f8434fe32
Lucas Smith <friedmicrochips@gmail.com>
2016-11-04 14:13:12 -0400

    Revert "Rewrite frontend to work with new endpoint"
    This reverts commit 21bebc9f8b3c645d27ef82da141df3ad600c1cc0.

--- admin/controllers/PlantViewController.js
+++ admin/controllers/PlantViewController.js
@@ -174,8 +174,7 @@ app.controller('PlantViewController', function($scope, CONFIG, countryFactory, $
 
         classificationLinkFactory.getPlantHierarchy($scope.plant.id).then(function(response) {
             console.log(response.data.data);
-            var data = response.data.data[0];
-            console.log(data);
+            var data = response.data.data;
             $scope.classification = {
                 class: "",
                 tribe: "",
@@ -184,12 +183,32 @@ app.controller('PlantViewController', function($scope, CONFIG, countryFactory, $
                 variety: "",
                 species: ""
             };
-                    $scope.classification.class = data.class;
-                    $scope.classification.tribe = data.tribe;
-                    $scope.classification.subtribe = data.subtribe;
-                    $scope.classification.genus = data.genus;
-                    $scope.classification.species = data.species;
-                    $scope.classification.variety = data.variety;
+
+            for (var i = 0; i < data.length; i++) {
+                var object = data[i];
+                var classificationName = object.name;
+                var scientificName = object.scientific_class_name;
+                if (classificationName == "class") {
+                    $scope.classification.class = scientificName;
+                }
+                if (classificationName == "tribe") {
+                    $scope.classification.tribe = scientificName;
+                }
+                if (classificationName == "subtribe") {
+                    $scope.classification.subtribe = scientificName;
+                }
+
+                if (classificationName == "genus") {
+                    $scope.classification.genus = scientificName;
+                }
+                if (classificationName == "species") {
+                    $scope.classification.species = scientificName;
+                }
+                if (classificationName == "variety") {
+                    $scope.classification.variety = scientificName;
+                }
+                console.log($scope.classification);
+            }
         });
 
         countryFactory.getCountries().then(function(response) {
