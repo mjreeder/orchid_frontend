f3c8889ed00c5ed8bf87ef9fa5c0d3712627512e
Lucas Smith <friedmicrochips@gmail.com>
2016-11-11 09:39:37 -0500

    Fix loading of login html template.

--- admin/config/routes.js
+++ admin/config/routes.js
@@ -11,7 +11,7 @@ app.config(function ($routeProvider, CONFIG) {
     }).
     when('/logout', {
             controller: 'LogoutViewController',
-            templateUrl: 'view/login.html'
+            templateUrl: 'views/login.html'
         }).
     when('/house', {
         controller: 'DisplayViewController',
