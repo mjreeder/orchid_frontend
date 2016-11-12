cfd8c495b65b9025855766fa88d49ed6f6ac4427
Kaleb Baldwin <kjbaldwin@bsu.edu>
2016-11-12 14:21:06 -0500

    adds the basic routes to app.js and on index.html

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-var orchid = angular.module('orchid', []);
+var orchidApp = angular.module('orchidApp', ['ui.router']);
+
+orchidApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/404');
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: './views/refactored/home.html'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: './views/refactored/about.html'
+        })
+        .state('contact', {
+            url: '/about',
+            templateUrl: './views/refactored/contact.html'
+        });
+});
