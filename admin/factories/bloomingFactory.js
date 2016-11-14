8a6d36a715fd88d87602e431b8c8d10eeb1ee2a2
Seth Winslow <swwinlow@bsu.eudu>
2016-11-14 15:36:08 -0500

    Updating Factories to work with Safari browsers

--- admin/factories/bloomingFactory.js
+++ admin/factories/bloomingFactory.js
@@ -5,7 +5,10 @@ app.factory('BloomingFactory', function($http, $rootScope) {
 
 
 
-    data.getBloomByPlantID = function(plant_id, page = 1) {
+    data.getBloomByPlantID = function(plant_id, page) {
+        if (page == undefined){
+            page = 1;
+        }
         return $http({
           method: "GET",
           url: baseUrl + "/plant_id/" + plant_id + "/page/" + page
--- admin/factories/healthFactory.js
+++ admin/factories/healthFactory.js
@@ -34,7 +34,10 @@ app.factory('HealthFactory', function($http, $rootScope) {
         })
     }
 
-    data.getHealthBtPlantID = function(id, page = 1){
+    data.getHealthBtPlantID = function(id, page){
+        if(page == undefined){
+            page = 1;
+        }
         return $http({
           method: "GET",
           url: baseUrl + '/plant_id/' + id + '/page/' + page
