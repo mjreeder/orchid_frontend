9ad542c934893e7d14d3d96cd490a125a0555e78
Lucas Smith <friedmicrochips@gmail.com>
2016-12-03 18:33:44 -0500

    Disable the calendar popup box when not on an iOS device which running Safari.

--- admin/app.js
+++ admin/app.js
@@ -2,3 +2,13 @@ var app = angular.module('orchid', [
     'ngRoute',
     'ui.bootstrap'
 ]);
+
+app.run(function($rootScope){
+  var setIosFlag = function(){
+    if(bowser.ios){
+      $rootScope.ios = true;
+    } else {
+      $rootScope.ios = false;
+    }
+  }
+})
