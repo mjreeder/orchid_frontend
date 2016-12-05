2d29312330e87094dc8ad2d0d81101ab1ce3acc3
Matt Reeder <mjreeder@bsu.edu>
2016-12-05 14:16:58 -0500

    Add capitalize filters to database page

--- admin/config/config.js
+++ admin/config/config.js
@@ -4,3 +4,9 @@ app.constant('CONFIG', {
         messageTimeout: 5000,
         maxFileSizeBits: 50 * 1000000
 });
+
+app.filter('capitalize', function() {
+    return function(input) {
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
+});
