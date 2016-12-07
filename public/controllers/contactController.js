99f8f0faf6bceee834b819c260f20c25115420ac
Seth Winslow <swwinlow@bsu.eudu>
2016-12-07 01:00:31 -0500

    Finished Contact Data. Need to hook up email

--- public/controllers/contactController.js
+++ public/controllers/contactController.js
@@ -3,8 +3,20 @@ orchidApp.controller('contactController', ['$scope', '$location', '$state', 'Ema
 
     $scope.submit = function(){
             console.log($scope.contact.subject);
+
+        var data = {
+            'html': $scope.contact.message,
+            'subject': $scope.contact.subject,
+            'from_email': $scope.contact.from_email,
+            'from_name': $scope.contact.from_name
+        }
+        console.log(data);
+
+        //MAKING THE REQUEST TO THE EMAIL SERVER
+
     }
 
 
-    console.log('contact controller');
+
+    //console.log('contact controller');
 }]);
