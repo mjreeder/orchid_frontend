587f2b87294d2369d27209d295283a1b2a30d5b5
Seth Winslow <swwinlow@bsu.eudu>
2017-01-31 14:21:30 -0500

    Still fixing commas

--- public/controllers/homePageController.js
+++ public/controllers/homePageController.js
@@ -1,6 +1,6 @@
 orchidApp.controller('HomePageController', function($scope, $location, $state, EmailFactory, PlantsFactory) {
 
-    $filter('number')(number, fractionSize);
+    //$filter('number')(number, fractionSize);
 
     PlantsFactory.getCount().then(function (response){
        $scope.count = response.data.data['COUNT(*)'];
