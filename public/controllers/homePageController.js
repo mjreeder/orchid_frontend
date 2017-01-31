2775ebf0b9fe37cfce1bcf3f31929f310897af97
Seth Winslow <swwinlow@bsu.eudu>
2017-01-31 14:18:58 -0500

    Added comma to the type

--- public/controllers/homePageController.js
+++ public/controllers/homePageController.js
@@ -1,5 +1,7 @@
 orchidApp.controller('HomePageController', function($scope, $location, $state, EmailFactory, PlantsFactory) {
 
+    $filter('number')(number, fractionSize);
+
     PlantsFactory.getCount().then(function (response){
        $scope.count = response.data.data['COUNT(*)'];
     }, function (response){
