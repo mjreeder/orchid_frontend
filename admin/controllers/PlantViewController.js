700dfcae5b7c854e54319d48d386ade860161922
Seth Winslow <swwinlow@bsu.eudu>
2017-04-18 01:46:54 -0400

    Add generic graph

--- admin/controllers/PlantViewController.js
+++ admin/controllers/PlantViewController.js
@@ -2555,4 +2555,98 @@ app.controller('PlantViewController', function($window, $scope, UserFactory, CON
         });
       }
     }
+
+
+    var data = 1;
+    Highcharts.chart('container', {
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
+        },
+        title: {
+            text: 'Height Versus Weight of 507 Individuals by Gender'
+        },
+        subtitle: {
+            text: 'Source: Heinz  2003'
+        },
+        xAxis: {
+            title: {
+                enabled: true,
+                text: 'Months'
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true,
+            max: 12,
+            allowDecimals: false,
+            mintickinterval: 1,
+            minRange: 10,
+            tickInterval:1,
+            min: 1
+        },
+        yAxis: {
+            title: {
+                text: 'Weight (kg)'
+            },
+            mintickinterval: 1,
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 100,
+            y: 70,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+            borderWidth: 1
+        },
+        plotOptions: {
+            scatter: {
+                marker: {
+                    radius: 5,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: '{point.x} Month, {point.y} kg'
+                }
+            }
+        },
+        series: [{
+            name: 'Hits',
+            color: 'rgba(223, 83, 83, .5)',
+            data: (function () {
+                // generate an array of random data
+                var data = [], time = (new Date()).getTime(), i;
+
+
+                for (i = 0; i <= 12; i += 1) {
+                    data.push([
+                        i,
+                        3
+                    ]);
+                }
+
+
+                return data;
+            }())
+
+        }, {
+            name: 'Months',
+            color: 'rgba(119, 152, 191, .5)',
+            data: []
+        }]
+    });
 });
